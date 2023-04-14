c662f265ccb6f02b0d3ee907f922a0350807cddc
Paulo Nascimento <paulo.nascimento@siemens-healthineers.com>
2023-04-14 17:02:07 -0300

    import cors and winston

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 import express from "express";
+import cors from "cors";
+import winston from "winston";
 import clientsRouter from "./routes/client.route.js";
 import suppliersRouter from "./routes/supplier.route.js";
 import productsRouter from "./routes/product.route.js";
 import salesRouter from "./routes/sale.route.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+const {combine, timestamp, label, printf} = winston.format;
+const myFormat = printf(({level, message, label, timestamp}) => `${timestamp} [${label}] ${level} ${message}`);
+
+global.logger = winston.createLogger({
+    level: "silly",
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File({filename: "store-api.log"}))(),
+    ],
+    format: combine(
+        label({ label: "store-api"}),
+        timestamp(),
+        myFormat
+    )
+
+});
 
 app.use("/client", clientsRouter);
 app.use("/sale", salesRouter);
