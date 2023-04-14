14ce7021b20b6a02fabc4c4bc75506de25d1451f
Paulo Nascimento <paulo.nascimento@siemens-healthineers.com>
2023-04-14 16:54:29 -0300

    set up root routes

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import clientsRouter from "./routes/client.route.js";
+import suppliersRouter from "./routes/supplier.route.js";
+import productsRouter from "./routes/product.route.js";
+import salesRouter from "./routes/sale.route.js";
 
 const app = express();
+
+app.use("/client", clientsRouter);
+app.use("/sale", salesRouter);
+app.use("/supplier", suppliersRouter);
+app.use("/product", productsRouter);
 
 app.listen(3000, () => console.log("API Started"));
