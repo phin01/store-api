7fa784c2eb75eb9bd0761101adcfafbf8cb3e4cb
phin01 <phin01@gmail.com>
2023-04-16 15:30:29 -0300

    create blank client service

---
+++ services/client.service.js
@@ -0,0 +1,7 @@
+import ClientRepository from "../repositories/client.repository.js";
+
+async function createClient(client) {
+    return await ClientRepository.insertClient(client);
+}
+
+export default { createClient }
