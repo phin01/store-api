a5e3e0d6d23c4eb5ca29f9c2ef3cebaaa9bfb676
phin01 <phin01@gmail.com>
2023-04-21 10:15:46 -0300

    implement function to insert client

--- repositories/client.repository.js
+++ repositories/client.repository.js
@@ -1,4 +1,13 @@
+import { connect } from "./db.js";
+
 async function insertClient(client) {
+
+    const conn = await connect();
+    const sql = "INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+    const values = [client.name, client.cpf, client.phone, client.email, client.address];
+    
+    const res = await conn.query(sql, values);
+    return res.rows[0];
 
 }
 
