17b19f48b7f3122a31de11bdbbc28b5f6f0cd785
phin01 <phin01@gmail.com>
2023-04-21 10:41:27 -0300

    include error handling and conn release for client insert

--- repositories/client.repository.js
+++ repositories/client.repository.js
@@ -3,12 +3,17 @@ import { connect } from "./db.js";
 async function insertClient(client) {
 
     const conn = await connect();
-    const sql = "INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *";
-    const values = [client.name, client.cpf, client.phone, client.email, client.address];
-    
-    const res = await conn.query(sql, values);
-    return res.rows[0];
-
+    try {
+        const sql = "INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+        const values = [client.name, client.cpf, client.phone, client.email, client.address];
+        const res = await conn.query(sql, values);
+        return res.rows[0];
+    } 
+    catch (error) {
+        throw error;
+    } finally {
+        conn.release();
+    }
 }
 
 export default {insertClient}
